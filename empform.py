910654a06b858ab92a8d252142a9d6978f300f16
fatemeh-fo <fotouhi.f20@gmail.com>
2024-07-17 14:11:11 +0330

    db complete

--- empform.py
+++ empform.py
@@ -1,93 +1,123 @@
 from logging import root
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
 
 import database
 from database import query
+
 
 def show_employee_form():
     root=tk.Tk()
     root.title("Employee Form")
 
     tk.Label(root, text="First Name").grid(row=0, column=0)
-    entry_fname = tk.Entry(root)
-    entry_fname.grid(row=0, column=1)
+    fname = tk.Entry(root)
+    fname.grid(row=0, column=1, sticky='we' , pady=10)
 
     tk.Label(root, text="Last Name").grid(row=1, column=0)
-    entry_lname = tk.Entry(root)
-    entry_lname.grid(row=1, column=1)
+    lname = tk.Entry(root)
+    lname.grid(row=1, column=1, sticky='we', pady=10)
 
     tk.Label(root, text="SSID").grid(row=2, column=0)
-    entry_ssid = tk.Entry(root)
-    entry_ssid.grid(row=2, column=1)
+    ssid = tk.Entry(root)
+    ssid.grid(row=2, column=1, sticky='we', pady=10)
 
-    tk.Label(root, text="Date of Birth (YYYY-MM-DD)").grid(row=3, column=0)
-    entry_dob = tk.Entry(root)
-    entry_dob.grid(row=3, column=1)
+    tk.Label(root, text="Date of Birth").grid(row=3, column=0)
+    dob = tk.Entry(root)
+    dob.grid(row=3, column=1, sticky='we', pady=10)
 
     tk.Label(root, text="Marital Status").grid(row=4, column=0)
-    entry_marital_status = tk.Entry(root)
-    entry_marital_status.grid(row=4, column=1)
-
-    tk.Label(root, text="Gender").grid(row=5, column=0)
+    marital_status = tk.StringVar()
     ttk.Combobox(
         state="readonly",
-        values=list(database.constants['gender'].values())
-    ).grid(row=5, column=1)
+        values=list(database.constants['marital_status'].values()),
+        textvariable=marital_status,
+    ).grid(row=4, column=1, sticky='we', pady=10)
+
+    tk.Label(root, text="Gender").grid(row=5, column=0)
+    gender = tk.StringVar()
+    ttk.Combobox(
+        state="readonly",
+        values=list(database.constants['gender'].values()),
+        textvariable=gender,
+    ).grid(row=5, column=1, sticky='we', pady=10)
 
 
     tk.Label(root, text="Position").grid(row=6, column=0)
+    emp_position = tk.StringVar()
     ttk.Combobox(
         state="readonly",
-        values=list(database.constants['emp_position'].values())
-    ).grid(row=6, column=1)
+        values=list(database.constants['emp_position'].values()),
+        textvariable=emp_position,
+    ).grid(row=6, column=1, sticky='we', pady=10)
 
     tk.Label(root, text="Department").grid(row=7, column=0)
-    entry_department = tk.Entry(root)
-    entry_department.grid(row=7, column=1)
+    department = tk.StringVar()
     ttk.Combobox(
         state="readonly",
-        values=list(database.constants['department'].values())
-    ).grid(row=7, column=1)
+        values=list(database.constants['department'].values()),
+        textvariable=department
+    ).grid(row=7, column=1, sticky='we', pady=10)
     
 
     tk.Label(root, text="Edu Prog").grid(row=8, column=0)
-    entry_department = tk.Entry(root)
-    entry_department.grid(row=8, column=1)
+    edu_prog = tk.StringVar()
     ttk.Combobox(
         state="readonly",
-        values=list(database.constants['edu_prog'].values())
-    ).grid(row=8, column=1)
+        values=list(database.constants['edu_prog'].values()),
+        textvariable=edu_prog,
+    ).grid(row=8, column=1, sticky='we', pady=10)
 
 
     tk.Label(root, text="Edu Level").grid(row=9, column=0)
-    entry_department = tk.Entry(root)
-    entry_department.grid(row=9, column=1)
+    edu_level = tk.StringVar()
     ttk.Combobox(
         state="readonly",
-        values=list(database.constants['edu_level'].values())
-    ).grid(row=9, column=1)
+        values=list(database.constants['edu_level'].values()),
+        textvariable=edu_level,
+    ).grid(row=9, column=1, sticky='we', pady=10)
 
     tk.Label(root, text="Address").grid(row=10, column=0)
-    entry_address = tk.Entry(root)
-    entry_address.grid(row=10, column=1)
+    address = tk.Entry(root)
+    address.grid(row=10, column=1, sticky='we', pady=10)
 
     tk.Label(root, text="Phone Number").grid(row=11, column=0)
-    entry_phone = tk.Entry(root)
-    entry_phone.grid(row=11, column=1)
+    phone = tk.Entry(root)
+    phone.grid(row=11, column=1, sticky='we', pady=10)
 
     tk.Label(root, text="Email Address").grid(row=12, column=0)
-    entry_email = tk.Entry(root)
-    entry_email.grid(row=12, column=1)
+    email = tk.Entry(root)
+    email.grid(row=12, column=1, sticky='we', pady=10)
 
-    tk.Button(root, text="Submit", command=lambda: submit_data(entry_fname.get(),
-    entry_lname.get(), entry_ssid.get(), entry_dob.get(), entry_marital_status.get(),
-    entry_gender.get(), entry_position.get(), entry_department.get(), entry_address.get(),
-    entry_phone.get(), entry_email.get())).grid(row=14, column=1)
+    tk.Label(root, text="Employee Contract").grid(row=13, column=0)
+    employee_type = tk.StringVar()
+    ttk.Combobox(
+        state="readonly",
+        values= ['Full-Time' , 'Part-Time'],
+        textvariable=employee_type,
+    ).grid(row=13, column=1, sticky='we', pady=10)
+
+    tk.Button(root, 
+              text="Submit", 
+              command=lambda: submit_data(
+                  fname.get(),
+                  lname.get(), 
+                  ssid.get(), 
+                  dob.get(), 
+                  marital_status.get(),
+                  gender.get(), 
+                  emp_position.get(), 
+                  department.get(), 
+                  address.get(),
+                  phone.get(), 
+                  email.get(),
+                  employee_type.get(),
+                  )).grid(row=16, column=1)
 
     root.mainloop()
 
-def submit_data(fname, lname, ssid, dob, marital_status, gender, position, department, address, phone, email):
+def submit_data(fname, lname, ssid, dob, marital_status, gender, position, department, address, phone, email, employee_type):
     print(f"First Name: {fname}")
     print(f"Last Name: {lname}")
     print(f"SSID: {ssid}")
@@ -99,5 +129,21 @@ def submit_data(fname, lname, ssid, dob, marital_status, gender, position, depar
     print(f"Address: {address}")
     print(f"Phone Number: {phone}")
     print(f"Email Address: {email}")
+    print(f"Employee Type: {employee_type}")
 
-    query(f'insert into employee (first_name, last_name, ssid, dob, marital_)')
+    if fname == '' or lname == '' or ssid == '' or dob == '' or marital_status == '' or \
+        gender == '' or position == '' or department == '' or address == '' or phone == '' or \
+        email == '' or employee_type == '':
+        return messagebox.showerror('Error!', 'All Fields are required!')
+
+    marital_status = find_constant_key('marital_status', marital_status)
+    gender = find_constant_key('gender', gender)
+    position = find_constant_key('emp_position', position)
+    department = find_constant_key('department', department)
+
+    query(f'insert into employee (first_name, last_name, ssid, dob, marital_status, gender, position, department, address, phone_number, email_address, employee_type) values ' + 
+          f'($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)',
+          [fname, lname, ssid, dob, marital_status, gender, position, department, address, phone, email, employee_type])
+
+def find_constant_key(table_name, value):
+    return [k for k, v in database.constants[table_name].items() if v == value][0]
